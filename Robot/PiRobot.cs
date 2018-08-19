8d5e10fc6aceec17f4d376ac97cf66987e5c731e
Steve Desmond <steve@vtsv.ca>
2018-08-19 14:00:57 -0400

    use separate motors for drive and flag

--- Robot/PiRobot.cs
+++ Robot/PiRobot.cs
@@ -1,28 +1,29 @@
 ﻿using System;
 using SimpleGPIO.Boards;
+using SimpleGPIO.Components;
 using SimpleGPIO.GPIO;
 
 namespace Robot.API
 {
     public class PiRobot : IRobot
     {
-        private readonly IPinInterface _driveMotor;
-        private readonly IPinInterface _flagMotor;
+        private readonly Motor _drive;
+        private readonly Motor _flag;
         private readonly IPinInterface _buzzer;
 
         public PiRobot(RaspberryPi pi)
         {
-            _driveMotor = pi.Pin13;
-            _flagMotor = pi.Pin15;
+            _drive = new Motor(pi.Pin11, pi.Pin13);
+            _flag = new Motor(pi.Pin33, pi.Pin35);
             _buzzer = pi.Pin40;
 
             var motorDriver = pi.Pin11;
             motorDriver.TurnOn();
         }
 
-        public void GoForward() => _driveMotor.TurnOnFor(TimeSpan.FromSeconds(1));
+        public void GoForward() => _drive.RunFor(TimeSpan.FromSeconds(1));
 
-        public void SpinFlag() => _flagMotor.TurnOnFor(TimeSpan.FromSeconds(1));
+        public void SpinFlag() => _flag.RunFor(TimeSpan.FromSeconds(1));
 
         public void Beep()
         {
